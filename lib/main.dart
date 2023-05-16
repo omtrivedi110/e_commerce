cc9c3c20c34e734dba9e80f2338023978ba19d22
r29 <roshnitailor47@gmail.com>
2023-05-16 11:43:28 +0530

    16-5-23

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => const MyApp();
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  bool theme = false;
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: AppBar(),
+    );
   }
 }
